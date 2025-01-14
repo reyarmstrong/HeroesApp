953076c0f76cd68b25bfba37e6742749263d7e3b
Rey Herrera <reylevig2@hotmail.com>
2025-01-13 22:02:37 -0600

    Initial commit

---
+++ src/app/heroes/pages/hero-page/hero-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-page',
+  standalone: false,
+  
+  templateUrl: './hero-page.component.html',
+  styles: ``
+})
+export class HeroPageComponent {
+
+}
