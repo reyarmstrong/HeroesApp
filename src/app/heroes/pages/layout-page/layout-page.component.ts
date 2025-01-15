044df7530f198d841eb6ce12048b6aa5ad61a555
Rey Herrera <reylevig2@hotmail.com>
2025-01-14 23:24:13 -0600

    Add Auth and Heroes default layout pages.

--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -3,10 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-layout-page',
   standalone: false,
-  
+
   templateUrl: './layout-page.component.html',
   styles: ``
 })
 export class LayoutPageComponent {
+
+  sidebarItems = [
+    {
+      label: 'List',
+      icon: 'label',
+      url: './list'
+    },
+    {
+      label: 'Add',
+      icon: 'add',
+      url: './new-hero'
+    },
+    {
+      label: 'Search',
+      icon: 'search',
+      url: './search'
+    },
+  ]
 
 }
