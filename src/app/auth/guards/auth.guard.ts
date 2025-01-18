91950be1a8fb30cb5d001e68027e3bd605e80d19
Rey Herrera <reylevig2@hotmail.com>
2025-01-18 00:46:26 -0600

    Add check auth status in Auth Guard.

--- src/app/auth/guards/auth.guard.ts
+++ src/app/auth/guards/auth.guard.ts
@@ -7,26 +7,44 @@ import {
   UrlSegment,
   GuardResult,
   MaybeAsync,
-  RouterStateSnapshot
+  RouterStateSnapshot,
+  Router
 } from '@angular/router';
+import { Observable, tap } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 @Injectable({providedIn: 'root'})
 export class AuthGuard implements CanMatch, CanActivate {
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  private checkAuthStatus(): boolean | Observable<boolean> {
+    return this.authService.checkAuthentication()
+      .pipe(
+        tap(isAuthenticated => {
+          if (!isAuthenticated) {
+            this.router.navigate(['./auth/login']);
+          }
+        }
+      )
+    );
+  }
 
   canMatch(route: Route, segments: UrlSegment[]): MaybeAsync<GuardResult> {
     console.log('CanMatch');
     console.log({ route, segments });
 
-    return false;
+    return this.checkAuthStatus();
   }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): MaybeAsync<GuardResult> {
     console.log('CanActivate');
     console.log({ route, state });
 
-    return false;
+    return this.checkAuthStatus();
   }
 
 }
